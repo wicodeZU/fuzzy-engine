25c24b050a409e2a9e524c9e9c68452c8db77c64
Carver <kerenketepela@gmail.com>
2022-05-18 18:35:04 +0300

    feat:home controller - create

--- api/controllers/home.controler.js
+++ api/controllers/home.controler.js
@@ -1,1 +1,19 @@
+const Home = require("../model/home")
+
 // controllers
+exports.createContent = async (req, res) => {
+  const { heading, paragraph } = req.body
+  try {
+    const data = await Home.create({
+      heading,
+      paragraph,
+    })
+    if (!data) {
+      return res.status(404).json({ message: "content not created" })
+    }
+    data.save()
+    res.status(200).json({ success: true, data })
+  } catch (err) {
+    return res.status(500).json({ message: err })
+  }
+}
