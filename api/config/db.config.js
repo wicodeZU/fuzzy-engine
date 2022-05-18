16b513bdabd776850d64f72ccd8938874a83b142
Carver <kerenketepela@gmail.com>
2022-05-18 18:35:32 +0300

    feat:connect to db

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,1 +1,13 @@
 // db connection
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const uri = process.env.MONGO_URI
+try {
+  mongoose.connect(uri)
+  console.log("mongodb connected")
+} catch (err) {
+  return console.log(err)
+}
