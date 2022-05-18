f2c963431ad2804b18fb218e07d370c77bc0e388
Carver <kerenketepela@gmail.com>
2022-05-18 15:33:08 +0300

    feat:server setup

--- api/index.js
+++ api/index.js
@@ -1,1 +1,9 @@
 // server
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+const port = 5000
+
+app.listen(port, () => console.log(`server running on ${port} `))
