a86cefebd600a12f77a81c02b5699ecd217b04a6
Carver <kerenketepela@gmail.com>
2022-05-18 18:36:01 +0300

    feat:register routes

--- api/index.js
+++ api/index.js
@@ -1,9 +1,19 @@
 // server
 const express = require("express")
+const dotenv = require("dotenv")
+
+dotenv.config()
 const app = express()
 
+//pull mongodb
+require("./config/db.config")
+
+//pull in router
+const home = require("./routes/home.routes")
+
 app.use(express.json())
+app.use("/create", home)
 
-const port = 5000
+const port = process.env.PORT
 
-app.listen(port, () => console.log(`server running on ${port} `))
+app.listen(port, () => console.log(`api running on ${port}`))
