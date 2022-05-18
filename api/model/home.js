ee42f6cb9d0d567d0ccf42e5818122a73a603374
Carver <kerenketepela@gmail.com>
2022-05-18 18:34:21 +0300

    feat:create a home schema

--- api/model/home.js
+++ api/model/home.js
@@ -1,1 +1,15 @@
 // db schemas
+const mongoose = require("mongoose")
+
+const homeSchema = mongoose.Schema({
+  heading: {
+    type: String,
+    required: true,
+  },
+  paragraph: {
+    type: String,
+    required: true,
+  },
+})
+const Home = mongoose.model("Home", homeSchema)
+module.exports = Home
