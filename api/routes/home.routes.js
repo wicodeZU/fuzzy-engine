5412f23b0966029bd266814054dd692993b4ecac
Carver <kerenketepela@gmail.com>
2022-05-18 18:34:45 +0300

    feat:home route

--- api/routes/home.routes.js
+++ api/routes/home.routes.js
@@ -1,1 +1,8 @@
 // views/routes
+const express = require("express")
+const { createContent } = require("../controllers/home.controler")
+const router = express.Router()
+
+router.route("/").post(createContent)
+
+module.exports = router
